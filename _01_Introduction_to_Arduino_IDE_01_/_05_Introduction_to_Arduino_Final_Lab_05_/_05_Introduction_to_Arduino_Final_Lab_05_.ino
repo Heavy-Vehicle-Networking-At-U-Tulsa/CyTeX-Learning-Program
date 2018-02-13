8200ca56082f4f5d42177c6096762468953c2c0c
hja685 <hayden-allen@utulsa.edu>
2018-02-13 12:16:22 -0600

    Added Final Lab and Merged Arduino Labs into Folder

---
+++ _01_Introduction_to_Arduino_IDE_01_/_05_Introduction_to_Arduino_Final_Lab_05_/_05_Introduction_to_Arduino_Final_Lab_05_.ino
@@ -0,0 +1,33 @@
+/*
+ * In this lab you are required to use your knowledge of functions from the previous labs. 
+ * 
+ * You are required to include a Serial monitor output, an led, a button/switch, and create
+ * a sketch that is different than the previously mentioned sketches.
+ * 
+ * Please prepare this to be demonstrated to your supervisor. 
+ * 
+ * -Maintenance
+ */
+
+//Enter variables that will not change below here. 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+/*
+ * Once you have this working please explain to your supervisor how your project works and let
+ * them review the code you have written. For any help please consult your previous labs and 
+ * the reference material include at www.arduino.cc
+ * 
+ * Thank you, I hope you have enjoyed this Getting Started portion. 
+ * 
+ * - Maintenance
+ */
